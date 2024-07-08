eee946ca223ffecde853414627691d1f46fc4094
adeebaah <adebaislam@iut-dhaka.edu>
2024-07-08 22:45:05 +0600

    fixed cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,33 +3,42 @@ class CartsController < ApplicationController
 
   def show
     @cart = current_user.cart || current_user.create_cart
-    @cart_items = @cart.cart_items.includes(:product)
+    @cart_items = @cart.cart_items.includes(:product).order("created_at DESC")
     respond_to do |format|
       format.html
       format.json { render json: { cart_items: @cart_items.as_json(include: :product) } }
     end
   end
+
   def add_item
     @cart = current_user.cart || current_user.create_cart
     @product = Product.find(params[:product_id])
     @cart_item = @cart.cart_items.find_by(product_id: @product.id, size: params[:size])
 
-    if @cart_item
-      @cart_item.quantity += 1
-    else
-      @cart_item = @cart.cart_items.build(product: @product, size: params[:size], quantity: 1)
-    end
+    if Stock.available?(@product.id, params[:size]) # Check if the stock is available
+      if @cart_item
+        @cart_item.quantity += 1
+      else
+        @cart_item = @cart.cart_items.build(product: @product, size: params[:size], quantity: 1)
+      end
 
-    if @cart_item.save
-      render json: { success: true, cart_items: @cart.cart_items.as_json(include: :product) }
+      if @cart_item.save
+        Stock.reduce!(@product.id, params[:size], 1) # Reduce the stock
+        render json: { success: true, cart_items: @cart.cart_items.as_json(include: :product) }
+      else
+        render json: { success: false, message: 'Error adding item to cart' }, status: :unprocessable_entity
+      end
     else
-      render json: { success: false }, status: :unprocessable_entity
+      render json: { success: false, message: 'Stock not available' }, status: :unprocessable_entity
     end
   end
 
   def increase_quantity
     cart_item = current_user.cart.cart_items.find(params[:id])
-    cart_item.update(quantity: cart_item.quantity + 1)
+    if Stock.available?(cart_item.product_id, cart_item.size)
+      cart_item.update(quantity: cart_item.quantity + 1)
+      Stock.reduce!(cart_item.product_id, cart_item.size, 1)
+    end
     redirect_to cart_path
   end
 
@@ -37,7 +46,9 @@ class CartsController < ApplicationController
     cart_item = current_user.cart.cart_items.find(params[:id])
     if cart_item.quantity > 1
       cart_item.update(quantity: cart_item.quantity - 1)
+      Stock.increase!(cart_item.product_id, cart_item.size, 1)
     else
+      Stock.increase!(cart_item.product_id, cart_item.size, cart_item.quantity)
       cart_item.destroy
     end
     redirect_to cart_path
@@ -45,11 +56,15 @@ class CartsController < ApplicationController
 
   def remove_item
     cart_item = current_user.cart.cart_items.find(params[:id])
+    Stock.increase!(cart_item.product_id, cart_item.size, cart_item.quantity)
     cart_item.destroy
     redirect_to cart_path
   end
 
   def clear
+    current_user.cart.cart_items.each do |cart_item|
+      Stock.increase!(cart_item.product_id, cart_item.size, cart_item.quantity)
+    end
     current_user.cart.cart_items.destroy_all
     redirect_to cart_path
   end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,3 +1,20 @@
 class Stock < ApplicationRecord
   belongs_to :product
+
+  def self.available?(product_id, size)
+    stock = where(product_id: product_id, size: size).first
+    stock && stock.amount > 0
+  end
+
+  def self.reduce!(product_id, size, quantity)
+    stock = find_by(product_id: product_id, size: size)
+    stock.amount -= quantity
+    stock.save!
+  end
+
+  def self.increase!(product_id, size, quantity)
+    stock = find_by(product_id: product_id, size: size)
+    stock.amount += quantity
+    stock.save!
+  end
 end
