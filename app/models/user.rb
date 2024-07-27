4f725ea7ede259c28810c8669ec389a46459408d
Adeba Islam <adebaislam@iut-dhaka.edu>
2024-07-28 00:04:25 +0600

    added validation and authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
+
   has_many :orders, dependent: :destroy
   has_many :reviews, dependent: :destroy
   has_one :cart, dependent: :destroy
@@ -6,23 +9,27 @@ class User < ApplicationRecord
   has_one :wishlist, dependent: :destroy
   after_create :initialize_wishlist
 
-
-
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must contain @" }
+  validates :phone, presence: true, length: { is: 11, message: "Must be 11 digits long" }
+  validates :address, presence: true, length: { minimum: 5, message: "Must be at least 5 characters long" }
+  validate :password_complexity
   def create_cart
     Cart.create(user: self)
   end
+
   def create_wishlist
     Wishlist.create(user: self)
   end
+
   def initialize_wishlist
     create_wishlist unless wishlist
   end
 
+  private
 
+  def password_complexity
+    return if password.blank? || password =~ /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/
 
-
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+    errors.add :password, 'Length should be at least 8 characters long and include: 1 uppercase, 1 lowercase and 1 digit'
+  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
   # config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
   ActionMailer::Base.smtp_settings = {
@@ -10,7 +10,7 @@ Rails.application.configure do
     :port                 => 587,
     :domain               => "gmail.com",
     :user_name            => "adebashanto15@gmail.com",
-    :password             => "vzmlsfgjkyydfmhx",
+    :password             => "zxkspdxxrzqzusqn ",
     :authentication       => "plain",
     # :ssl                  => true,
     # :tsl                  => true,
